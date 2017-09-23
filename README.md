832ac18daaa30d05d7661a5ebccf2500b391c68a
NLGRF <nongrace6654@gmail.com>
2017-09-24 00:10:22 +0700

    Clock PWA

---
+++ README.md
@@ -0,0 +1,23 @@
+# Vuejs PWA Clock Inspiration Samsung S8 (log Screen)
+
+> A Vue.js project pwa
+![./screen.png](./screen.png)
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+"# Web-PWA-Vue" 
---
+++ src/components/Hello.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div class='centered1'> {{ updatedTime1 }} </div>
+    <div class='centered2'> {{ updatedTime2 }} </div>
+    <div class='centered3'> {{ updateDate }} </div>
+    <!-- <div class="battery"> {{ updatedBattery }} % </div> -->
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      updatedTime1: '',
+      updatedTime2: '',
+      updateDate: ''
+    }
+  },
+  methods: {
+    updatedTime () {
+      this.updatedTime1 = moment().format('H')
+      this.updatedTime2 = moment().format('mm')
+      this.updateDate = moment().format('dddd, Do MMMM YYYY')
+    }
+  },
+  mounted () {
+    setInterval(() => {
+      this.updatedTime()
+    }, 1000)
+  }
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+
+.centered1 {
+  position: fixed;
+  top: 45%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+  color: #fff;
+  font-size: 50px;
+  padding-left: 0px;
+}
+
+.centered2 {
+  position: fixed;
+  top: 52%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+  color: #fff;
+  font-size: 50px;
+  padding-left: 0px;
+}
+.centered3 {
+  position: fixed;
+  top: 60%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+  color: #fff;
+  font-size: 12px;
+  padding-left: 0px;
+}
+
+.battery {
+  position: fixed;
+  top: 63%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+  color: #fff;
+  font-size: 12px;
+  padding-left: 0px;
+}
+
+</style>
